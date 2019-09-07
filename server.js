c15db7d192eafa61ab158e70640573f04d54e027
morralex <morr.alex@outlook.com>
2019-09-07 11:00:42 -0700

    port change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var socket = require('socket.io');
 
 var app = express();
 
-var PORT = process.argv.PORT || 8080;
+var PORT = process.env.PORT || 8080;
 
 
 // Sets up the Express app to handle data parsing
